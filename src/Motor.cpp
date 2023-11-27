1a82435eb28853a9423b1bf1d243961a942c67f6
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2023-11-26 18:55:43 -0800

    Update Motor.cpp
    Adding Motor Wiring comment

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -8,21 +8,18 @@ Servo Motor3;
 Servo Motor4;
 
 void Motor_Init(){
-    pinMode(26,OUTPUT);
-    pinMode(25,OUTPUT);
-    pinMode(27,OUTPUT);
-    pinMode(14,OUTPUT);
-    Motor1.attach(26,1000,2000);
-    Motor2.attach(25,1000,2000);
-    Motor3.attach(27,1000,2000);
-    Motor4.attach(14,1000,2000);
-    //ledcSetup(0, 50, 12);
-    //ledcAttachPin(26,0);
+    pinMode(26,OUTPUT);//Front Right Motor
+    pinMode(25,OUTPUT);//Front Left Motor
+    pinMode(27,OUTPUT);//Back Left Motor
+    pinMode(14,OUTPUT);//Back Right Motor
+    Motor1.attach(26,1000,2000);//Front Right Motor
+    Motor2.attach(25,1000,2000);//Front Left Motor
+    Motor3.attach(27,1000,2000);//Back Left Motor
+    Motor4.attach(14,1000,2000);//Back Right Motor
 }
 
 void Motor_Start(){
     int ThrottleValue = RC_VALUES[2];
-    //ledcWrite(0,ThrottleValue);
     ThrottleValue = map(ThrottleValue,1050,2000,0,180);
     Motor1.write(ThrottleValue);
     Motor2.write(ThrottleValue);
